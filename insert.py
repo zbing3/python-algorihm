51e2119fa207698c285c361c67f6deb49bb5a6f7
swxy <swxymile@163.com>
2014-09-07 18:47:47 +0800

    add shell sort

--- insert.py
+++ insert.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-  
+
 def insertSort(a):
 	l = len(a)
 	i = 1
@@ -43,16 +45,16 @@ def bInsertSort(a):
 
 
 def tInsertSort(a):
-	#set another array c for store the sorted list
+	#设置新的列表存储数据
 	length = len(a) 
 	c = a[:]
-	first = length
+	first = length #把数组设置为循环数组
 	final = 0
 	for i in range(1, len(a)):
 		if a[i] < c[0]:
 			c[first-1] = a[i]
 			low = first
-			high = length-1
+			high = length-1 #折半插入排序插入
 			while low <= high:
 				m = (low + high) // 2
 				if a[i] < c[m]:
@@ -60,7 +62,7 @@ def tInsertSort(a):
 				else:
 					low = m + 1
 			for j in range(first, high+1):
-				c[j-1], c[j] = c[j], c[j-1] #the small element move to right
+				c[j-1], c[j] = c[j], c[j-1] #将小元素移动到右边
 			first -= 1
 
 		else:
@@ -74,14 +76,31 @@ def tInsertSort(a):
 				else:
 					low = m + 1
 			for j in range(final, high, -1):
-				c[j+1], c[j] = c[j], c[j+1] #the bigger element move to left
+				c[j+1], c[j] = c[j], c[j+1] #t移动大元素到左边
 			final += 1
 	return c
 
 
+def shellInsert(a, dk):
+	length = len(a)
+	for x in xrange(dk+1,length):
+		if a[x] < a[x-dk]:
+			for y in xrange(x, dk-1, -dk):
+				if a[y] < a[y-dk]:
+					a[y], a[y-dk] = a[y-dk], a[y]
+				else:
+					break
+def shellSort(a):
+	length = len(a)
+	dk = len(a)
+	while(dk >= 1):
+		dk = dk // 2
+		shellInsert(a, dk)
+
+
 if __name__ == '__main__':
 	import random
-	b = [random.randint(1,1000) for i in xrange(200) ]
+	b = [random.randint(1,100) for i in xrange(20) ]
 	print b
 	insertSort(b)
 	print 'insertSort 1 ',b
@@ -100,4 +119,9 @@ if __name__ == '__main__':
 	random.shuffle(b)
 	print 'shuffle b ', b
 	print 'tInsertSort ', tInsertSort(b)
+
+	random.shuffle(b)
+	print 'shuffle b ', b
+	shellSort(b)
+	print 'shellSort ', b
 	
